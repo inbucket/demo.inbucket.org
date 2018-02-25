4e6fe58657d74ce2459a6978d417e3e478b75395
James Hillyerd <james@hillyerd.com>
2018-02-25 15:05:54 -0800

    bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# bootstrap.sh
+
+REPO="https://github.com/jhillyerd/demo.inbucket.org"
+WORK_DIR="/etc/docker/compose"
+TARGET="inbucket"
+
+set -ex
+
+if ! test -e "$WORK_DIR/$TARGET"; then
+  mkdir -p "$WORK_DIR"
+  git clone "$REPO" "$WORK_DIR/$TARGET"
+fi
