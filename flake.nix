462d4e9387afeb68fefb0f6cb9f824695c8771b5
James Hillyerd <james@hillyerd.com>
2024-07-06 09:48:38 -0700

    update flake
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 pkgs.mkShell {
   buildInputs =
     let
-      python-env = pkgs.python310.withPackages (p: [ p.google-auth ]);
+      python-env = pkgs.python311.withPackages (p: [ p.google-auth ]);
     in
     with pkgs; [
       ansible
