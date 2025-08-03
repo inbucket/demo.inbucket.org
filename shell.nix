c60763091bcaf96cb76daf3ef2f538b628bbfafa
James Hillyerd <james@hillyerd.com>
2025-08-03 14:35:49 -0700

    update for 3.1.0 release
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 pkgs.mkShell {
   buildInputs =
     let
-      python-env = pkgs.python311.withPackages (p: [ p.google-auth ]);
+      python-env = pkgs.python3.withPackages (p: [ p.google-auth ]);
     in
     with pkgs; [
       ansible
